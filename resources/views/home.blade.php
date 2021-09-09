e269cbaac2c78e4650077df8fdc9959b7a696fba
pHedi3 <federico.gualdi3@gmail.com>
2021-09-09 16:35:07 +0200

    problema risolto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="./css/app.css">
 </head>
 <body>
     <div class="container">
         <div class="row">
             @foreach ($allMovies as $movie)
-                <div class="col-3">
+                <div class="col-3 tag">
                     <h1>{{$movie->title}}</h1>
                     <h2>{{$movie->original_title}}</h2>
                     <h3>{{$movie->nationality}}</h3>
                     <h3>{{$movie->date}}</h3>
-                    <h3>{{$movie->vote}}</h3>
+                    <h2>{{$movie->vote}}</h2>
                 </div>
             @endforeach
         </div>
